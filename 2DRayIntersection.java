748fad33536d0072ca8a166ae7627defda76340f
Milan Saxena <msaxena@berkeley.edu>
2016-09-20 10:46:41 -0700

    added documentation to 2DRayIntersection and MeasurementDevice

--- 2DRayIntersection.java
+++ 2DRayIntersection.java
@@ -2,17 +2,19 @@
 public class 2DRayIntersection {
 	/* Returns the intersection of two 2D Rays */
 
-
+	/* Two devices, deviceA and deviceB, and their intersection point, intersectPostion. */
 	private MeasurementDevice deviceA;
 	private MeasurementDevice deviceB;
 	private Position intersectPosition;
 
+	/* Constructor that initializes deviceA, deviceB, intersectPosition. */
 	public 2DRayIntersection(MeasurementDevice deviceOne, MeasurementDevice deviceTwo) {
 		deviceA = deviceOne;
 		deviceB = deviceTwo;
 		intersectPosition = new Position(0,0);
 	}
 
+	/* Sets intersectPosition to the intersect point of deviceA and deviceB. */
 	public void set2DRayIntersection() {
 		double angleA = deviceA.getOffsetAngle();
 		double angleB = deviceB.getOffsetAngle();
@@ -35,34 +37,39 @@ public class 2DRayIntersection {
 		intersectPosition = intersection;
 	}
 
+	/* Changes the offset angle for DeviceA and recalculates intersect position. */
 	public void changeAngleForDeviceA(double angle) {
 		deviceA.changeOffsetAngle(angle);
 		set2DRayIntersection();
 	}
 
-
+	/* Changes the offset angle for DeviceB and recalculates intersect position. */
 	public void changeAngleForDeviceB(double angle) {
 		deviceB.changeOffsetAngle(angle);
 		set2DRayIntersection();
 	}
 
+	/* Returns intersectionPosition for deviceA and deviceB. */
 	public Position get2DRayIntersection() {
 		return intersectPosition;
 	}
 
+	/* Returns X Coordinate of intersectionPoint. */
 	public double get2DRayIntersectionXCoordinate() {
 		return intersectPosition.getXCoordinate();
 	}
 
-
+	/* Returns Y Coordinate of intersectionPoint. */
 	public double get2DRayIntersectionYCoordinate() {
 		return intersectPosition.getYCoordinate();
 	}
 
+	/* Returns measurement device: deviceA. */
 	public MeasurementDevice getDeviceA() {
 		return deviceA;
 	}
 
+	/* Returns measurement device: deviceB. */
 	public MeasurementDevice getDeviceB() {
 		return deviceB;
 	}
--- MeasurementDevice.java
+++ MeasurementDevice.java
@@ -1,27 +1,35 @@
 public Class MeasurementDevice {
+	/* Models Measurement Device, which can detect objects at certain angle 
+		and at a certain sampling frequency. */
 	
+	/* The position, the offset angle, and the sampling frequency of a Measurement Device.*/
 	private Position devicePosition;
 	private double offsetAngle;
 	private double samplingFrequency;
 
+	/* Initializes devicePostion, offsetAngle, and samplingFrequency. */
 	public MeasurementDevice(Position pos, double angle, double frequency) {
 		devicePosition = pos;
 		offsetAngle = angle;
 		samplingFrequency = frequency;
 	}
 
+	/* Changes offsetAngle to angle. */
 	public void changeOffsetAngle(double angle) {
-		offsetAngle += angle;
+		offsetAngle = angle;
 	}
 
+	/* Returns the position of the device, devicePosition. */
 	private Position getDevicePosition() {
 		return devicePosition;
 	}
 
+	/* Returns the offset angle of the device, offsetAngle. */
 	private double getOffsetAngle() {
 		return offsetAngle;
 	}
 
+	/* Returns the sampling frequency of the device, samplingFrequency. */
 	private double getSamplingFrequency() {
 		return samplingFrequency;
 	}
